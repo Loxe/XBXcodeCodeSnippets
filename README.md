ee8da4c0b16aa3bfbbd152818ab9f8eaafa4af85
张彬彬 <zhangbb@ufotosofot.com>
2022-02-18 14:34:19 +0800

    change read me

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # XBXcodeCodeSnippets
 
 ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+# Install
+
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Loxe/XBXcodeCodeSnippets/master/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ git clone https://github.com/Loxe/XBXcodeCodeSnippets.git "${tmp_path}"
 code_snippet_path="${HOME}/Library/Developer/Xcode/UserData/CodeSnippets"
 if [ ! -d "${code_snippet_path}" ];then
   mkdir "${code_snippet_path}"
-else
-  echo "文件夹已经存在"
 fi
+
 softfiles=$(ls $tmp_path)
 for sfile in ${softfiles}
 do 
